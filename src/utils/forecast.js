7e3e90a33b29c2d48f49a905931dc6b188ea21bc
AOne <admin@positivesquare.com>
2020-01-24 20:33:20 +0000

    Typo

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently <b>' + body.currently.temperature + ' degress</b> out with a low of <b>' + body.daily.data[0].temperatureLow + ' degrees</b> and a high of <b>' + body.daily.data[0].temperatureHigh + ' degrees.</b> There is a ' + body.currently.precipProbability + '% chance of rain.')
+            const currentlyTemp = body.currently.temperature
+            const temperatureLow = body.daily.data[0].temperatureLow
+            const temperatureHigh = body.daily.data[0].temperatureHigh
+            callback(undefined, body.daily.data[0].summary + ' It is currently <b>' + currentlyTemp.bold() + ' degress</b> out with a low of <b>' + temperatureLow.bold() + ' degrees</b> and a high of ' + temperatureHigh.bold() + ' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
